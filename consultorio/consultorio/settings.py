68384b63baca55eb8766f76bdc52a6c036feee7d
Martín Arroyo <maredarro@gmail.com>
2025-10-12 14:52:19 -0300

    Agregados roles Empleado, Subdirector y Director y modelo de usuario

--- consultorio/consultorio/settings.py
+++ consultorio/consultorio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'informacion',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,14 @@ WSGI_APPLICATION = 'consultorio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'usuarioscesfam',   
+        'USER': 'postgres',          
+        'PASSWORD': '1234',          
+        'HOST': '127.0.0.1',        
+        'PORT': '5432',             
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- consultorio/informacion/models.py
+++ consultorio/informacion/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    ROL_CHOICES = [
+        ('Empleado', 'Empleado'),
+        ('Subdirector', 'Subdirector'),
+        ('Director', 'Director'),
+    ]
+    rol = models.CharField(max_length=20, choices=ROL_CHOICES, default='Empleado')
+
+    def __str__(self):
+        return f"{self.nombre} - {self.rol}"
+    
